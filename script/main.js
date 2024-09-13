ef310f0d1e858c696a2d724f424005acf1e864a0
Mena Ehab <127118141+commanderYT123@users.noreply.github.com>
2024-09-13 12:51:28 +0300

    Add function to render task list dynamically

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-let input = document.getElementById("task");
+let input = document.getElementById("input");
 let submit = document.getElementById("add");
 let tasks = document.getElementById("tasks");
 let taskList = [];
@@ -18,5 +18,16 @@ function addTask(taskText) {
     completed: false,
   };
   taskList.push(task);
+  addElements(taskList);
 }
-console.log(taskList);
+// console.log(taskList);
+
+function addElements(taskList) {
+  tasks.innerHTML = "";
+  taskList.forEach((task) => {
+    let div = document.createElement("div");
+    div.className = "task";
+    div.setAttribute("data-id", task.id);
+    div.appendChild(document.createTextNode(task.title));
+  });
+}
